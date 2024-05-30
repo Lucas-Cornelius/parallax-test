f27ff2bdf31abc3739ba2ceab397888eef548913
Lucas <lucasv.cornelius@gmail.com>
2024-05-30 17:18:15 -0300

    adjusting the parallax

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const control = document.querySelector('.control');
 const btn = document.querySelector('.page-control');
 
 const background = document.querySelector('.background-moon');
+const backgroundStars = document.querySelector('.background-stars');
 const title = document.querySelector('.title');
 const branches = document.querySelector('.branches');
 const bigTrees = document.querySelector('.tree-zero');
@@ -16,22 +17,27 @@ let jaFoi = false;
 document.addEventListener('scroll', () => {
     const value = window.scrollY;
     if (value<900) {
+        treeOne.style.marginTop = value * .4 + 'px';
         treeOne.style.marginLeft = value * .1 + 'px';
         treeOne.style.opacity = 1 - (value/1000);
     
+        treeTwo.style.marginTop = value * .4 + 'px';
         treeTwo.style.marginRight = value * .1 + 'px';
         treeTwo.style.opacity = 1 - (value/1000);
     
-        bigTrees.style.marginTop = value * .9 + 'px';
-    
-        branches.style.marginTop = value * .2 + 'px';
-    
+        bigTrees.style.marginBottom = value * 1.1 + 'px';
+        bigTrees.style.transform = `scale(${1 - (value * .0001)})`;
+        
+        branches.style.marginTop = value * .6 + 'px';
+        
         screenWidthController(value);
-
-        background.style.marginTop = value * 1.8 + 'px';
+        
+        backgroundStars.style.transform = `scale(${1 - (value * .0005)})`;
+        background.style.transform = `scale(${1 - (value * .0005)})`;
+        background.style.marginTop = value * 2 + 'px';
         background.style.opacity = 1 - (value/400);
     
-        title.style.marginTop = value * .7 + 'px';
+        title.style.marginTop = value * 4 + 'px';
         title.style.opacity = 1 - (value/800);
         console.log(value)
     } else {
@@ -42,8 +48,10 @@ document.addEventListener('scroll', () => {
 function screenWidthController (value) {
     if (value > 400) {
         background.style.display = 'none';
+        title.style.display = 'none';
     } else {
         background.style.display = 'block';
+        title.style.display = 'block';
     };
 
     if (value > 800) {
